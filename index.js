6d5cdf0ff3e644b0c2397df68ea68e5aab07d53a
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2022-08-23 17:28:27 -0700

    step three, updated newItem to create inventoryItem

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ function newItem(url, left, bottom){
     let item = newImage(url, left, bottom)
     item.addEventListener('click', function(){
         item.remove()
+        let inventoryItem = document.createElement('img')
+        inventoryItem.src = url
+        inventory.append(inventoryItem)
     })
 }
 
